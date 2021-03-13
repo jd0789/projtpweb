9ae3dd1ffb32da65ef23875971bfc86194918fb3
A389177 Jerome Degroote <jerome.degroote@socgen.com>
2021-03-13 07:55:51 +0100

    add front Livre - part 1

--- src/main/java/com/example/demo/livres/resource/LivreResource.java
+++ src/main/java/com/example/demo/livres/resource/LivreResource.java
@@ -2,9 +2,12 @@ package com.example.demo.livres.resource;
 
 import com.example.demo.livres.bdd.Livre;
 import com.example.demo.livres.bdd.LivreRepository;
+import com.example.demo.personnes.process.Personne;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -22,5 +25,12 @@ public class LivreResource {
         List<Livre> livres = new ArrayList<>();
         livreRepository.findAll().forEach(livres::add);
         return livres;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Livre creeerLivre(Livre l) {
+        return livreRepository.save(l);
     }
 }
--- src/main/resources/public/hello.js
+++ src/main/resources/public/hello.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
+	let $listeLivres = $("#listLivres");
+	let $listePersonnes = $("#listPersonnes");
+	let $selectLivres = $("#selectLivres");
 
     $.get("http://localhost:8080/api/personnes",function(resp){
-    	$.each(resp, function(index, item) {
-    		console.log(item);
-    		$('#list').append('<li id="personne-'+item.id+'" class="list-group-item">'+item.prenom+' '+item.age+' <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >X</button></li>');
-    	});
+		resp.forEach( p => appendToListPersonne(p));
     });
-    
-    $('#list').delegate("li button", "click", function() {
-    	console.log(this);
-    	var elemid = $(this).parent().attr('id');
+
+	$.get("http://localhost:8080/api/livres",function(resp){
+		resp.forEach( l => {
+			appendToListLivre(l);
+			appendToSelects(l);
+		});
+	});
+
+	$listePersonnes.on("click", "li button", function() {
+    	let elemid = $(this).parent().attr('id');
     	
     	$.ajax({
 		    type: "DELETE",
@@ -21,9 +27,9 @@ $(document).ready(function() {
 		});	
     });
     
-    $('#addbtn').click(function(){
-		var prenom = $('#prenom-input').val();
-		var age = $('#age-input').val();
+    $('#addbtnPersonne').click(function(){
+		let prenom = $('#prenom-input').val();
+		let age = $('#age-input').val();
 		
 		$.ajax({
 		    type: "POST",
@@ -31,15 +37,55 @@ $(document).ready(function() {
 		    data: JSON.stringify({ "prenom": prenom, "age" : age }),
 		    contentType: "application/json; charset=utf-8",
 		    dataType: "json",
-		    success: function(data){
-		    	console.log(data);
-		    	$('#list').append('<li id="personne-'+data.id+'" class="list-group-item">'+prenom+' '+age+' <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >X</button></li>');
-		    }
+		    success: function(data){ appendToListPersonne(data) }
 		});
-		
 		
 		$('#prenom-input').val('');
 		$('#age-input').val('');
 		return false;
 	});
+
+	$('#addbtnLivre').click(function(){
+		let livre = {
+			titre: $('#title-input').val(),
+			nbPage: $('#nbpage-input').val(),
+			auteur: {
+				nom: $('#nom-auteur-input').val(),
+				prenom: $('#prenom-auteur-input').val()
+			}
+		};
+
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8080/api/livres",
+			data: JSON.stringify(livre),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function(data){
+				appendToListLivre(data);
+				appendToSelects(data);
+			}
+		});
+
+		$('#title-input').val('');
+		$('#nbpage-input').val('');
+		$('#nom-auteur-input').val('');
+		$('#prenom-auteur-input').val('');
+		return false;
+	})
+
+	/* Ajoute un élément li dans la liste de livres*/
+	function appendToListLivre(livre) {
+		$listeLivres.append(`<li id="livre-${livre.id}" class="list-group-item">${livre.titre} - ${livre.nbPages} - ${livre.auteur.nom} ${livre.auteur.prenom} <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >X</button> </li>`);
+	}
+
+	/* Ajoute un élément li dans le select des livres*/
+	function appendToSelects(livre) {
+		$selectLivres.append(`<option value="${livre.id}">${livre.titre}</option>`)
+		}
+
+	/* Ajoute un élément li dans la liste de personne*/
+	function appendToListPersonne(personne) {
+		$listePersonnes.append(`<li id="personne-${personne.id}" class="list-group-item">${personne.prenom} ${personne.age} <button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >X</button></li>`);
+	}
 });
